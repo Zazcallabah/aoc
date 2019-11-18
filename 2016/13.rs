7b6dbdefcae219aceefe0e4fb1385cdd0915aab0
Peter Hamberg <zazcallabah@gmail.com>
2019-11-18 14:13:31 +0100

    animate a star for 13

--- 2016/13.rs
+++ 2016/13.rs
@@ -10,22 +10,47 @@ fn is_wall(fav: u32, node: Node) -> bool {
 		fav as i32 + node.0 * node.0 + 3 * node.0 + 2 * node.0 * node.1 + node.1 + node.1 * node.1;
 	calcd.count_ones() % 2 == 1
 }
+
+fn get_walls(fav: u32, from: Node, to: Node) -> Vec<Vec<bool>> {
+	let dx = (to.0 - from.0) as usize;
+	let dy = (to.1 - from.1) as usize;
+	let mut rows = Vec::with_capacity(dy);
+	for sy in from.1..to.1 {
+		let mut row = Vec::with_capacity(dx);
+		for sx in from.0..to.0 {
+			row.push(is_wall(fav, (sx, sy)));
+		}
+		rows.push(row);
+	}
+	rows
+}
+
+fn wallslice(walls: &Vec<Vec<bool>>) -> Vec<&[bool]> {
+	walls.iter().map(|w| &w[..]).collect()
+}
+
 fn draw(fav: u32, from: Node, to: Node) -> String {
 	let w = get_walls(fav, from, to);
-	let row_size = to.0 - from.0;
 	let mut s = String::new();
-	for row in w.chunks(row_size as usize) {
+	for row in w {
 		let rowstr: String = row.iter().map(|b| if *b { '#' } else { ' ' }).collect();
 		s.push_str(&rowstr);
 		s.push('\n');
 	}
 	s
 }
-fn add_path(grid: &Vec<bool>, path: &Vec<Node>, row_size: usize) -> String {
+fn add_path(grid: &[&[bool]], path: &[Node], signal: Option<&[Node]>) -> String {
 	let mut output = Vec::with_capacity(grid.len());
-	for (y, row) in grid.chunks(row_size).enumerate() {
+	for (y, row) in grid.iter().enumerate() {
 		for (x, n) in row.iter().enumerate() {
-			if path.contains(&(x as i32, y as i32)) {
+			let current = (x as i32, y as i32);
+			if let Some(s) = signal {
+				if s.contains(&current) {
+					output.push('X');
+					continue;
+				}
+			}
+			if path.contains(&current) {
 				output.push('.');
 			} else {
 				output.push(if *n { '#' } else { ' ' });
@@ -34,15 +59,6 @@ fn add_path(grid: &Vec<bool>, path: &Vec<Node>, row_size: usize) -> String {
 		output.push('\n');
 	}
 	output.into_iter().collect()
-}
-fn get_walls(fav: u32, from: Node, to: Node) -> Vec<bool> {
-	let mut collected = Vec::new();
-	for sy in from.1..to.1 {
-		for sx in from.0..to.0 {
-			collected.push(is_wall(fav, (sx, sy)));
-		}
-	}
-	collected
 }
 
 fn heuristic(node: Node, goal: Node) -> i32 {
@@ -91,7 +107,7 @@ fn neighbours(node: Node) -> [Node; 4] {
 	]
 }
 
-fn a_star(start: Node, goal: Node, fav: u32) -> Vec<Node> {
+fn a_star(start: Node, goal: Node, fav: u32, walls: Option<&[&[bool]]>) -> Vec<Node> {
 	let mut open = vec![start];
 	let mut parent: HashMap<Node, Node> = HashMap::new();
 	let mut g_score: HashMap<Node, i32> = HashMap::new();
@@ -124,27 +140,54 @@ fn a_star(start: Node, goal: Node, fav: u32) -> Vec<Node> {
 				}
 			}
 		}
+
+		if let Some(w) = walls {
+			let keys: Vec<Node> = parent.keys().map(|k| k.clone()).collect();
+			let output = add_path(&w, &keys, Some(&open));
+			println!(
+				"{}{}{}",
+				termion::clear::All,
+				termion::cursor::Goto(1,1),
+				output
+			);
+			std::thread::sleep(std::time::Duration::from_millis(50));
+		}
 	}
 
 	panic!("no path found")
 }
 fn main() {
 	let fav = 1352;
-	println!("before\n{}", draw(fav, (0, 0), (50, 50)));
-	let walls = get_walls(fav, (0, 0), (50, 50));
-	let path = a_star((1, 1), (31, 39), fav);
+
+	let walls = get_walls(fav, (0, 0), (42, 42));
+	let slicedwalls = wallslice(&walls);
+	let path = a_star((1, 1), (31, 39), fav, Some(&slicedwalls));
+	let part1path = add_path(&slicedwalls, &path, Some(&[(1, 1), (31, 39)]));
+
+	let fill = depth_first(vec![(1, 1)], vec![(1, 1)], 50, fav);
+	let part2path = add_path(&slicedwalls, &fill, None);
 
 	println!(
-		"\n\n{}\npart 1 total {} nodes, {} moves",
-		add_path(&walls, &path, 50),
+		"{}{}part 1 total {} nodes, {} moves{}part 2 total {} nodes\n",
+		termion::clear::All,
+		termion::cursor::Goto(1, 2),
 		&path.len(),
-		&path.len() - 1
+		&path.len() - 1,
+		termion::cursor::Right(20),
+		fill.len()
 	);
 
-	let fill = depth_first(vec![(1,1)],vec![(1,1)],50,fav);
-
-	println!("\n\n{}\npart 2 total {} nodes", add_path(&walls,&fill,50), fill.len());
-
+	let mut part2lines = part2path.lines();
+	for line in part1path.lines() {
+			if let Some(b) = part2lines.next() {
+				println!(
+					"{}{}{}",
+					line,
+					termion::cursor::Right(5),
+					b
+				);
+			}
+	}
 }
 
 #[cfg(test)]
@@ -169,38 +212,41 @@ mod tests {
 
 	#[test]
 	fn test_draw_path() {
-		let walls = get_walls(10, (0, 0), (10, 4));
-		let r = a_star((1, 1), (7, 4), 10);
+		let w = get_walls(10, (0, 0), (10, 4));
+		let walls = wallslice(&w);
+		let r = a_star((1, 1), (7, 4), 10, None);
 		assert_eq!(
 			" # #### ##\n .#  #   #\n#... ##   \n###.# ### \n",
-			add_path(&walls, &r, 10)
+			add_path(&walls, &r, None)
 		);
 	}
 
 	#[test]
 	fn test_depth_search() {
-		let walls = get_walls(10, (0, 0), (10, 4));
-		let r = depth_first(vec![(1,1)],vec![(1,1)], 1, 10);
-		assert_eq!(3,r.len());
+		let w = get_walls(10, (0, 0), (10, 4));
+		let walls = wallslice(&w);
+		let r = depth_first(vec![(1, 1)], vec![(1, 1)], 1, 10);
+		assert_eq!(3, r.len());
 		assert_eq!(
 			" # #### ##\n..#  #   #\n#.   ##   \n### # ### \n",
-			add_path(&walls, &r, 10)
+			add_path(&walls, &r, None)
 		);
 	}
 	#[test]
 	fn test_depth_search2() {
-		let walls = get_walls(10, (0, 0), (10, 4));
-		let r = depth_first(vec![(1,1)],vec![(1,1)], 3, 10);
-		assert_eq!(6,r.len());
+		let w = get_walls(10, (0, 0), (10, 4));
+		let walls = wallslice(&w);
+		let r = depth_first(vec![(1, 1)], vec![(1, 1)], 3, 10);
+		assert_eq!(6, r.len());
 		assert_eq!(
 			".# #### ##\n..#  #   #\n#... ##   \n### # ### \n",
-			add_path(&walls, &r, 10)
+			add_path(&walls, &r, None)
 		);
 	}
 
 	#[test]
 	fn test_astar() {
-		let r = a_star((1, 1), (7, 4), 10);
+		let r = a_star((1, 1), (7, 4), 10, None);
 
 		assert_eq!(12, r.len());
 		assert_eq!(
