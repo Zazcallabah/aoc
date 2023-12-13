9395ba9b31c543aa253febc9e944efe9970c71f5
Peter Hamberg <zazcallabah@gmail.com>
2023-12-13 11:15:17 +0100

    add day 13 part 1

--- 2023/13.rs
+++ 2023/13.rs
@@ -33,11 +33,43 @@ impl Pattern {
         }
         false
     }
+    fn is_horizontal_candidate(&self, x: usize, row: isize) -> bool {
+        let s: Vec<u8> = self
+            .data
+            .iter()
+            .map(|st| *st.as_bytes().get(x as usize).unwrap())
+            .collect();
+        let s_size = s.len() as isize;
+
+        for ix in 0isize.. {
+            let top_ix: isize = row - ix;
+            if top_ix < 0 {
+                return true;
+            }
+            let bottom_ix: isize = (row + 1) + ix;
+            if bottom_ix >= s_size {
+                return true;
+            }
+            if s[top_ix as usize] != s[bottom_ix as usize] {
+                return false;
+            }
+        }
+        false
+    }
 
     fn is_vertical_valid(&self, x: isize) -> bool {
         //row 0 already checked
         for row in 1..self.height {
             if !self.is_vertical_candidate(x, row) {
+                return false;
+            }
+        }
+        return true;
+    }
+    fn is_horizontal_valid(&self, row: isize) -> bool {
+        //col 0 already checked
+        for x in 1..self.width {
+            if !self.is_horizontal_candidate(x, row) {
                 return false;
             }
         }
@@ -59,6 +91,33 @@ impl Pattern {
         }
 
         None
+    }
+
+    fn find_horizontal(&self) -> Option<usize> {
+        let mut candidates: Vec<usize> = Vec::new();
+        for r in 0..(&self.height - 1) {
+            if self.is_horizontal_candidate(0, r as isize) {
+                candidates.push(r);
+            }
+        }
+
+        for c in candidates {
+            if self.is_horizontal_valid(c as isize) {
+                return Some(c);
+            }
+        }
+
+        None
+    }
+
+    fn summarise(&self) -> usize {
+        if let Some(c) = self.find_vertical() {
+            return c + 1;
+        }
+        if let Some(r) = self.find_horizontal() {
+            return (r + 1) * 100;
+        }
+        panic!("invalid pattern")
     }
 
     fn new_vec(data: &str) -> Vec<Pattern> {
@@ -88,7 +147,13 @@ impl Pattern {
     }
 }
 fn main() {
-    let data = std::fs::read_to_string("2018/7.txt").unwrap();
+    let data = std::fs::read_to_string("2023/13.txt").unwrap();
+    let p = Pattern::new_vec(&data);
+
+    println!(
+        "summarized: {}",
+        p.iter().map(|f| f.summarise()).sum::<usize>()
+    );
 }
 
 #[cfg(test)]
@@ -110,6 +175,24 @@ mod tests {
 ..##..###
 #....#..#
 ";
+    #[test]
+    fn test_can_summarize() {
+        let p = Pattern::new_vec(TEST_DATA);
+
+        assert_eq!(405usize, p.iter().map(|f| f.summarise()).sum());
+    }
+    #[test]
+    fn test_find_vertical() {
+        let p = Pattern::new_vec(TEST_DATA);
+        assert_eq!(Some(4), p[0].find_vertical());
+        assert_eq!(None, p[1].find_vertical());
+    }
+    #[test]
+    fn test_find_horizontal() {
+        let p = Pattern::new_vec(TEST_DATA);
+        assert_eq!(None, p[0].find_horizontal());
+        assert_eq!(Some(3), p[1].find_horizontal());
+    }
     #[test]
     fn test_can_determine_is_candidate() {
         let p = Pattern::new_vec(TEST_DATA);
